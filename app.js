53ebaef353cb6c7b0d3fc4275f6bd92481e6beff
Igor Leandro <igtag.code@gmail.com>
2025-01-29 03:18:47 -0300

    Array criado e card 2 finalizado com algumas alterações a serem feitas

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 let amigos = [];
+//Função para exibir uma mensagem no HTML usando javascript
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+function addAmigos() {
+  //usar o valor do campo de entrada de texto
+
+  let amigoInput = document.querySelector("#amigo").value.trim().toLowerCase();
+
+  if (amigoInput === "") {
+    exibirTextoNaTela(".section-title", "Por favor insira o nome de um amigo");
+    return;
+  }
+  if (amigos.includes(amigoInput)) {
+    exibirTextoNaTela(
+      ".section-title",
+      `O nome ${amigoInput} já foi adicionado`
+    );
+    return;
+  }
+  //add amigos no array
+  amigos.push(amigoInput);
+
+  //Resetar o campo de entrada para um novo nome e foca o input
+  document.querySelector("#amigo").value = "";
+  document.querySelector("#amigo").focus;
+  console.log(amigos);
+}
